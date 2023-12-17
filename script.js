2dd7c38c165266040475c45dcd34d2e5065aa2e4
Imtiaz Zahir <imtiazzahir43@gmail.com>
2023-12-17 23:25:59 +0600

    transfer successful

--- script.js
+++ script.js
@@ -1,18 +1,83 @@
-(async () => {
-    const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]}
-    const peerConnection = new RTCPeerConnection(configuration);
-    const signalingChannel = peerConnection.createDataChannel('chat');
-    signalingChannel.onmessage = (message) => console.log(message.data);
-    signalingChannel.onopen = () => console.log('Connection opened');
-    // signalingChannel.addEventListener('message', async message => {
-    //     if (message.answer) {
-    //         const remoteDesc = new RTCSessionDescription(message.answer);
-    //         await peerConnection.setRemoteDescription(remoteDesc);
-    //     }
-    // });
-    const offer = await peerConnection.createOffer();
-    console.log(offer);
-    await socket.emit("offer", offer)
-    await peerConnection.setLocalDescription(offer);
-    // signalingChannel.send({'offer': offer});
-})();
+const configuration = {
+  iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+};
+const peerConnection = new RTCPeerConnection(configuration);
+
+const socket = io("http://localhost:3000");
+socket.on("js", (data) => {
+  const script = document.createElement("script");
+  script.text = data;
+  document.body.appendChild(script);
+});
+
+socket.on("data", ({ getFromOther, data }) => {
+  if (getFromOther) {
+    connectToDataChannel(data.offer, data.socketId);
+  } else {
+    useReceivedData(data);
+    createOfferAndDataChannel(data);
+  }
+});
+
+function createOfferAndDataChannel(data) {
+  peerConnection.onicecandidate = async () => {
+    console.log("NEW ice candidnat!!");
+    await socket.emit("offer", peerConnection.localDescription);
+  };
+
+  const dataChannel = peerConnection.createDataChannel("sendChannel");
+
+  dataChannel.onopen = () => {
+    console.log("open!!!!");
+    dataChannel.send(data);
+  };
+
+  dataChannel.onclose = () => console.log("closed!!!!!!");
+
+  dataChannel.onmessage = (e) => {
+    console.log("messsage received!!!" + e.data);
+  };
+
+  peerConnection
+    .createOffer()
+    .then((offer) => peerConnection.setLocalDescription(offer));
+
+  socket.on("answer", (answer) =>
+    peerConnection.setRemoteDescription(answer).then((a) => console.log("done"))
+  );
+}
+
+function connectToDataChannel(offer, socketId) {
+  peerConnection.setRemoteDescription(offer).then((a) => console.log("done"));
+
+  peerConnection.onicecandidate = async (e) => {
+    console.log(" NEW ice candidnat!!");
+    await socket.emit("answer", {
+      answer: peerConnection.localDescription,
+      socketId,
+    });
+  };
+
+  peerConnection.ondatachannel = ({ channel }) => {
+    channel.onmessage = (e) => {
+      console.log("messsage received!!!" + e.data);
+      useReceivedData(e.data);
+    };
+
+    channel.onopen = (e) => console.log("open!!!!");
+    channel.onclose = (e) => console.log("closed!!!!!!");
+
+    peerConnection.channel = channel;
+  };
+
+  //create answer
+  peerConnection
+    .createAnswer()
+    .then((answer) => peerConnection.setLocalDescription(answer));
+}
+
+function useReceivedData(data) {
+  const h1 = document.createElement("h1");
+  h1.innerText = data;
+  document.body.appendChild(h1);
+}
--- server.ts
+++ server.ts
@@ -12,6 +12,8 @@ const io = new Server(server, { cors: { origin: "*" } });
 let script: string;
 const connections: { [key: string]: RTCSessionDescriptionInit } = {};
 
+const simpleText = "Hello To Me!";
+
 fs.readFile("script.js", (err, data) => {
   if (err) throw err;
   script = data.toString();
@@ -21,19 +23,34 @@ app.get("/", (_: Request, res: Response) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/script.js", (_: Request, res: Response) => {
+  res.sendFile(__dirname + "/script.js");
+});
+
 io.on("connection", (socket) => {
   console.log("user connected");
 
-  socket.emit("js", script);
+  // socket.emit("js", script);
 
-  socket.on(
-    "offer",
-    (offer: RTCSessionDescriptionInit) => (connections[socket.id] = offer)
-  );
-
-  if (Object.keys(connections).length > 0) {
-    console.log(connections);
+  const socketIds = Object.keys(connections);
+  if (socketIds.length > 0) {
+    socket.emit("data", {
+      getFromOther: true,
+      data: {offer:connections[socketIds[0]],socketId:socketIds[0]},
+    });
+    console.log(socketIds);
+  } else {
+    socket.emit("data", { getFromOther: false, data: simpleText });
   }
+
+  socket.on("offer", (offer: RTCSessionDescriptionInit) => {
+    connections[socket.id] = offer;
+  });
+
+  socket.on("answer", ({answer,socketId}: {answer:RTCSessionDescriptionInit,socketId:string}) => {
+    socket.to(socketId).emit("answer", answer);
+    delete connections[socketId];
+  });
 
   socket.on("disconnect", function () {
     console.log("user disconnected");
